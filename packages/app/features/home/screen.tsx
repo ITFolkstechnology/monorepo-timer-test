aca0ac7315ca20b49c1ac45d5c302e9f6b2ff63a
arvo-palma <alvaro@80lines.com>
2023-11-24 16:40:04 -0300

    feat: create timer display layout

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,17 +1,28 @@
-import { H1, P } from 'app/design/typography'
-import { View } from 'app/design/view'
+import { useQuery } from '@tanstack/react-query';
+import { View } from 'app/design/view';
+import { Platform } from 'react-native';
+import { TIMER_PATH, getTime } from './actions';
+import TimerDisplay from './components/timer-display';
 
 
 export function HomeScreen() {
+  const timeFetch = {
+    queryFn: () => getTime(),
+    queryKey: [TIMER_PATH],
+  };
+
+  const { data, status } = useQuery(timeFetch);
+
+  setTimeout(() => console.log(status),1000)
+
+  const baseContainerStyle = Platform.select({
+    web: 'items-center justify-center',
+    native: 'items-center justify-center'
+  })
+
   return (
-    <View className="flex-1 items-center justify-center p-3">
-      <H1>Welcome to PureCodeX</H1>
-      <View className="max-w-xl">
-        <P className="text-center">
-          Here is a basic starter to provide you with a quick overview of the
-          project structure. Please, check the readme file for more details.
-        </P>        
-      </View>      
+    <View className={`flex-1 p-3 w-full h-full max-w-lg ${baseContainerStyle}`}>
+      <TimerDisplay value={data || 0} />   
     </View>
   )
 }
