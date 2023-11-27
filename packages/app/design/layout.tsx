9ae190c7cb89eafcb2810cb690c4bf6724f8ddcf
arvo-palma <alvaro@80lines.com>
2023-11-27 15:13:54 -0300

    feat: create column

--- packages/app/design/layout.tsx
+++ packages/app/design/layout.tsx
@@ -1,4 +1,5 @@
-import { View } from 'react-native'
 import { styled } from 'nativewind'
+import { View } from 'react-native'
 
 export const Row = styled(View, "flex-row")
+export const Column = styled(View, "flex-col")
