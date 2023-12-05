5fcc2eeae859994c37211d90690ea77b1f127b3a
Lima2006 <gabrielchagaslima2015@gmail.com>
2023-12-05 18:07:52 -0300

    chore: install and setup react query

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -1,5 +1,12 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SafeArea } from './safe-area'
 
+const queryClient = new QueryClient()
+
 export function Provider({ children }: { children: React.ReactNode }) {
-  return <SafeArea>{children}</SafeArea>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SafeArea>{children}</SafeArea>
+    </QueryClientProvider>
+  )
 }
