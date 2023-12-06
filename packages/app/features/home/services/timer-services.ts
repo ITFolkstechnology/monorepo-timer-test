391b93d8c64d234844938024e41356e28c434151
Lima2006 <gabrielchagaslima2015@gmail.com>
2023-12-06 19:54:02 -0300

    refactor: unify timer services in a single file

---
+++ packages/app/features/home/services/timer-services.ts
@@ -0,0 +1,37 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { api } from 'app/features/core/api'
+import { TimerDataType } from '../types'
+
+export const TIMER_QUERY_KEY = ['currentTime']
+
+type GetCurrentTimeType = () => Promise<TimerDataType>
+export const getCurrentTime: GetCurrentTimeType = async () => {
+  try {
+    const { data } = await api.get<TimerDataType>('currentTime')
+    return data
+  } catch (error) {
+    throw console.log(error)
+  }
+}
+export const useGetCurrentTime = () =>
+  useQuery({
+    queryKey: TIMER_QUERY_KEY,
+    queryFn: getCurrentTime,
+  })
+
+type UpdateCurrentTimeType = (params: TimerDataType) => Promise<void>
+export const updateCurrentTime: UpdateCurrentTimeType = async (params) => {
+  try {
+    await api.put<ResponseType>('currentTime', params)
+  } catch (error) {
+    throw console.log(error)
+  }
+}
+export const useUpdateCurrentTime = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: updateCurrentTime,
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: TIMER_QUERY_KEY }),
+  })
+}
