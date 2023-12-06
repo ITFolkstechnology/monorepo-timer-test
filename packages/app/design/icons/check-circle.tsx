7af18e8464eff5d2b4dab0d18c1652eeb83b50db
Lima2006 <gabrielchagaslima2015@gmail.com>
2023-12-06 18:03:45 -0300

    feat: add sync indicator icons

---
+++ packages/app/design/icons/check-circle.tsx
@@ -0,0 +1,4 @@
+import { CheckCircle } from "@nandorojo/heroicons/20/solid";
+import { SvgProps } from "react-native-svg";
+
+export const CheckCircleIcon: React.FC<SvgProps> = props => <CheckCircle {...props} />
